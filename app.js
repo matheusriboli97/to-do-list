58da55d1d2bc5d2ec315d37c33e993731744283a
Matheus Riboli <matheusvor@gmail.com>
2020-06-13 01:05:59 -0300

    middleware

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require ('express')
+const checkListRouter = require('./src/routes/checklist')
 
 const app = express()
+app.use(express.json()) 
 
-app.get('/', (req, res) => {
-    res.send('<h1> Minha lista de tarefas </h1>')
-})
+app.use('/checklists',checkListRouter) // Coloco o /checklists aqui e não preciso colocar no routes, Isso é um middleware! Todo app.use() recebe como param um middleware, ali em cima inclusive express.json() é um 
 
 app.listen(3000, () => {
     console.log("Servidor iniciado!")
 })
+
