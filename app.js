018f3d0138f48ab258cdf13b386cf50ddf592080
Matheus Riboli <matheusvor@gmail.com>
2020-06-17 11:28:44 -0300

    Incluindo atualização e remoção de checklists

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require ('express')
 const path = require('path')
-
 const checkListRouter = require('./src/routes/checklist')
 const rootRouter = require('./src/routes/index')
-
+const methodOverride = require('method-override')
 require('./config/database')
 
 
 const app = express()
 app.use(express.json()) 
 app.use(express.urlencoded({extended: true})) // Permite que pegue dados da url
+app.use(methodOverride('_method', {methods: ['POST', 'GET']})) // Método que permite enviar formulario interno com put e delete também, coisa que o html5 nao permite por padrão, atua sobre o post e o get pq eu deixei explicito
 app.use(express.static(path.join(__dirname, 'public'))) //Arquivos estáticos vão ficar na pasta public
 app.set('views', path.join(__dirname, 'src/views')) //Estou dizendo que as views estão em src/views
 app.set('view engine', 'ejs')
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -3,62 +3,74 @@ const express = require('express')
 const router = express.Router()
 
 const Checklist = require('../models/checklist')
+const { findById } = require('../models/checklist')
 
 router.get('/', async (req, res) => {
-    try{
+    try {
         let checklists = await Checklist.find({})
-        res.status(200).render('checklists/index', { checklists: checklists}) //Estou devolvendo para a minha view uma listagem de todas as checklists, uso ela no ejs
-    } catch(error) {
-        res.status(500).render('pages/error', {error: 'Erro ao exibir as listas'})
+        res.status(200).render('checklists/index', { checklists: checklists }) //Estou devolvendo para a minha view uma listagem de todas as checklists, uso ela no ejs
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Erro ao exibir as listas' })
     }
 })
 
-router.get('/new', async(req, res) => {
-    try{
+router.get('/new', async (req, res) => {
+    try {
         let checklist = new Checklist()
-        res.status(200).render('checklists/new', {checklist: checklist})
-    }catch(error){
-        res.status(500).render('pages/error', {errors: 'Erro ao carregar o form'})
+        res.status(200).render('checklists/new', { checklist: checklist })
+    } catch (error) {
+        res.status(500).render('pages/error', { errors: 'Erro ao carregar o form' })
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).render('checklists/edit', { checklist: checklist })
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Erro ao exibir a edição de lista de tarefas' })
     }
 })
 
 router.post('/', async (req, res) => {
     let { name } = req.body.checklist
-    let checklist = new Checklist({name})
-    try{
+    let checklist = new Checklist({ name })
+    try {
         await checklist.save()
         res.redirect('/checklists')
-    } catch(error){
-        res.status(422).render('checklists/new', {checklists: {...checklist, error}})
+    } catch (error) {
+        res.status(422).render('checklists/new', { checklists: { ...checklist, error } })
     }
 })
 
 router.get('/:id', async (req, res) => {
-        try{
-            let checklist = await Checklist.findById(req.params.id)
-            res.status(200).render('checklists/show', { checklist: checklist}) 
-        }catch(error){
-            res.status(500).render('pages/error', {error: 'Erro ao exibir as listas de tarefas'})
-        }
+    try {
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).render('checklists/show', { checklist: checklist })
+    } catch (error) {
+        res.status(500).render('pages/error', { error: 'Erro ao exibir as listas de tarefas' })
+    }
 })
 
 router.put('/:id', async (req, res) => {
-    let { name } = req.body
-    
-    try{
-        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true}) //O new: true serve para devolver a tarefa já atualizada, mas não tem problema em termos de aplicação deixar sem 
-        res.status(200).json(checklist)
-   }catch(error){
-        res.status(422).json.error
-   }
+    let { name } = req.body.checklist
+    let checklist = await Checklist.findById(req.params.id)
+
+    try {
+        await checklist.update({ name })
+        res.redirect('/checklists')
+    } catch (error) {
+        let errors = error.errors
+        res.status(422).render('checklists/edit', {checklist: {...checklist, errors}})
+    }
 })
 
 router.delete('/:id', async (req, res) => {
     try {
         let checklist = await Checklist.findByIdAndRemove(req.params.id)
-        res.status(200).json(checklist)
+        res.redirect('/checklists')
     } catch (error) {
-        res.status(422).json.error
+        res.status(500).render('pages/error',{ error: 'Erro ao deletar lista de tarefas' })
     }
 })
 
